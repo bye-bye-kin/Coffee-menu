cdbe1efd0543b4897d711b58fbff34215ba49283
bye-bye-kin <21431061ms@tama.ac.jp>
2020-06-16 23:32:42 +0900

    'コミット'

--- main.js
+++ main.js
@@ -1,44 +1,39 @@
+$('.btn').click(function(){
+  $(this).toggleClass("click");
+  $('.sidebar').toggleClass("show");
+});
 
-    $('.btn').click(function(){
-      $(this).toggleClass("click");
-      $('.sidebar').toggleClass("show");
-    });
+$('.btn').click(function(){
+  $('.contents-wrapper').toggleClass("show");
+});
 
-    $('.btn').click(function(){
-      $('.contents-wrapper').toggleClass("show");
-    });
+$('.feat-btn').click(function(){
+  $('nav ul .feat-show').toggleClass("show");
+  $('nav ul .first').toggleClass("rotate");
+});
+$('.serv-btn').click(function(){
+  $('nav ul .serv-show').toggleClass("show1");
+  $('nav ul .second').toggleClass("rotate");
+});
+$('nav ul li').click(function(){
+  $(this).addClass("active").siblings().removeClass("active");
+});
 
-    $('.feat-btn').click(function(){
-      $('nav ul .feat-show').toggleClass("show");
-      $('nav ul .first').toggleClass("rotate");
-    });
-    $('.serv-btn').click(function(){
-      $('nav ul .serv-show').toggleClass("show1");
-      $('nav ul .second').toggleClass("rotate");
-    });
-    $('nav ul li').click(function(){
-      $(this).addClass("active").siblings().removeClass("active");
-    });
+//recipes end///////////////////////////////////////////////////////////
 
-  //recipes end///////////////////////////////////////////////////////////
-  
-  $('.mugs-contents>div').hide();
-    $('.mugs-contents>div').first().slideDown();
-    $('.mugs-tabs-bottons span').click(function(){
-      var thisclass=$(this).attr('class');
-    $('#lamp').removeClass().addClass('#lamp').addClass(thisclass);
-    $('.mugs-contents>div').each(function(){
-      if($(this).hasClass(thisclass)){
-        $(this).fadeIn(800);
-      }
-      else{
-        $(this).hide();
-      }
-    });
-  });
+$('.mugs-contents>div').hide();
+$('.mugs-contents>div').first().slideDown();
+$('.mugs-tabs-bottons span').click(function(){
+  var thisclass=$(this).attr('class');
+$('#lamp').removeClass().addClass('#lamp').addClass(thisclass);
+$('.mugs-contents>div').each(function(){
+  if($(this).hasClass(thisclass)){
+    $(this).fadeIn(800);
+  }
+  else{
+    $(this).hide();
+  }
+});
+});
 
-  // mugs end///////////////////////////////////////////////////////////////////
-
-  
-
-  
+// mugs end///////////////////////////////////////////////////////////////////
